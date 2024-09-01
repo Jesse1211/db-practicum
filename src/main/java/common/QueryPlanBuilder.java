c595b8c1c35c609c292650cf9efdfc6952641898
Jesse Liu <lzhliu@ucdavis.edu>
2024-08-31 20:07:28 -0400

    style: fix style

--- src/main/java/common/QueryPlanBuilder.java
+++ src/main/java/common/QueryPlanBuilder.java
@@ -5,33 +5,22 @@ import net.sf.jsqlparser.statement.Statement;
 import operator.*;
 
 /**
- * Class to translate a JSQLParser statement into a relational algebra query
- * plan. For now only
- * works for Statements that are Selects, and specifically PlainSelects. Could
- * implement the visitor
- * pattern on the statement, but doesn't for simplicity as we do not handle
- * nesting or other complex
+ * Class to translate a JSQLParser statement into a relational algebra query plan. For now only
+ * works for Statements that are Selects, and specifically PlainSelects. Could implement the visitor
+ * pattern on the statement, but doesn't for simplicity as we do not handle nesting or other complex
  * query features.
  *
- * <p>
- * Query plan fixes join order to the order found in the from clause and uses a
- * left deep tree
- * join. Maximally pushes selections on individual relations and evaluates join
- * conditions as early
- * as possible in the join tree. Projections (if any) are not pushed and
- * evaluated in a single
- * projection operator after the last join. Finally, sorting and duplicate
- * elimination are added if
+ * <p>Query plan fixes join order to the order found in the from clause and uses a left deep tree
+ * join. Maximally pushes selections on individual relations and evaluates join conditions as early
+ * as possible in the join tree. Projections (if any) are not pushed and evaluated in a single
+ * projection operator after the last join. Finally, sorting and duplicate elimination are added if
  * needed.
  *
- * <p>
- * For the subset of SQL which is supported as well as assumptions on semantics,
- * see the Project
+ * <p>For the subset of SQL which is supported as well as assumptions on semantics, see the Project
  * 2 student instructions, Section 2.1
  */
 public class QueryPlanBuilder {
-  public QueryPlanBuilder() {
-  }
+  public QueryPlanBuilder() {}
 
   /**
    * Top level method to translate statement to query plan
